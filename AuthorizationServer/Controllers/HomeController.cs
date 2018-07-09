d5c4157e518ef88487e09423fa47019e20d214d9
Atoyr <lessironglance@gmail.com>
2018-07-09 23:25:14 +0900

    .Net Core 2.1 React-Redux Most Lover

--- AuthorizationServer/Controllers/HomeController.cs
+++
@@ -1,7 +0,0 @@
-using Microsoft.AspNetCore.Mvc;
-
-namespace AuthorizationServer.Controllers{
-    public class HomeController : Controller{
-        public IActionResult Index() => File("./index.html", "text/html");
-    }
-}
