c497414e6226ec5032029bca38c0953d6852384d
Atoyr <lessironglance@gmail.com>
2018-07-10 00:03:28 +0900

    不要な処理を削除

--- AuthorizationServer/Controllers/OAuth2Controller.cs
+++ AuthorizationServer/Controllers/OAuth2Controller.cs
@@ -40,17 +40,6 @@ namespace AuthorizationServer.Controllers
         [HttpPost, Consumes("application/x-www-form-urlencoded")]
         public async Task<IActionResult> PostAsync([FromForm]IDictionary<string,string> value)
         {
-            foreach(var kv in value)
-            {
-                Console.WriteLine($"{kv.Key} : {kv.Value}");
-            }
-            Console.WriteLine(value);
-            var param = new Dictionary<string,string>();
-            param.Add("access_token","hoge"); 
-            //param.Add("token_type",value.grant_type);
-            //param.Add("expires_in",value.grant_type);
-            //param.Add("refresh_token",value.grant_type);
-            //param.Add("scope",value.grant_type);
             return await Task.Run(() => Json(_context.ResponsePostAsync(value))); 
         }
     }
