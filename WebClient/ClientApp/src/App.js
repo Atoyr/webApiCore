c60a300e0571690fc06a786cb7689d9dfff393c8
Atoyr <lessironglance@gmail.com>
2018-07-18 23:18:32 +0900

    APP削除対応

--- WebClient/ClientApp/src/App.js
+++ WebClient/ClientApp/src/App.js
@@ -1,6 +1,5 @@
 ﻿import React from 'react';
 import { Route, Switch} from 'react-router';
-import App from './containers/App';
 import Auth from './containers/Auth';
 import Login from './containers/Login';
 import Index from './containers/Index';
