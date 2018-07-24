f314045558bd5f9b42a1d0d913bb110f29317223
r.uchiyama <r.uchiyama@sint.co.jp>
2018-07-24 09:18:45 +0900

    Settingをドロップダウンに変更

--- WebClient/ClientApp/src/containers/Header.jsx
+++ WebClient/ClientApp/src/containers/Header.jsx
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import { Navbar, Nav, NavItem, Button } from 'react-bootstrap';
+import { Navbar, Nav, NavItem, Button ,MenuItem, NavDropdown} from 'react-bootstrap';
 import { connect } from 'react-redux';
 import {Link} from 'react-router-dom';
 import { fetchLoginStateAsync, requestLogoutAsync } from '../actions/auth';
@@ -14,9 +14,11 @@ const Header = ({requestLogoutAsync}) => {
                 </Navbar.Brand>
             </Navbar.Header>
             <Nav pullRight>
-                <NavItem onClick={requestLogoutAsync}>
+                <NavDropdown title="setting">
+                    <MenuItem onClick={requestLogoutAsync}>
                     Logout
-                </NavItem>
+                    </MenuItem>
+                </NavDropdown>
             </Nav>
         </Navbar>)
 }
