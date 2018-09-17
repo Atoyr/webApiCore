9e2f00c4c4d6cedbaeaa627127e7b15224f4817c
Atoyr <lessironglance@gmail.com>
2018-09-17 22:15:52 +0900

    とりあえずコミット

--- WebClient/ClientApp/src/middleware/jwtMiddleware.js
+++ WebClient/ClientApp/src/middleware/jwtMiddleware.js
@@ -1,21 +1,18 @@
 import decode from 'jwt-claims';
 
-export function　jwt({dispatch, getstate}) {
-    retunr next => action => {
-        if (typeof action === 'function') {
-            getstate().auth && getstate().auth.token){
-                var exp = decode(getstate().auth.token).exp;
-                if (exp && (moment(exp) - moment(Date.now()) < 5000)) {
-
-                    // make sure we are not already refreshing the token
-                    if (!getState().auth.freshTokenPromise) {
-                        return refreshToken().then(() => next(action));
-                    } else {
-                        return getState().auth.freshTokenPromise.then(() => next(action));
-                    }
+export const jwt = store => next => action => async() => {
+    if (typeof action === 'function') {
+        if(getstate().auth && getstate().auth.token && !auth.isFetching) {
+            
+            if (exp && (moment(exp) - moment(Date.now()) < 5000)) {
+                // make sure we are not already refreshing the token
+                if (!getState().auth.freshTokenPromise) {
+                    return await refreshToken().then(() => next(action));
+                } else {
+                    return getState().auth.freshTokenPromise.then(() => next(action));
                 }
             }
         }
-        return next(action);
     }
+    return next(action);
 }
